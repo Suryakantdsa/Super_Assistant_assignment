e480a309bc2768dceae353da98da711b02e11d56
Suryakantdsa <dassuryakantadas@gmail.com>
2023-07-25 20:58:33 +0530

    setup completed  commit

---
+++ Frontend/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ClozeQuestion from "./src/ClozeQuestion";
+
+const App = () => {
+  return (
+    <div>
+      <ClozeQuestion />
+    </div>
+  );
+};
+
+export default App;
